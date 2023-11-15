cc723517614ddd099d8b9a09904202a2874e5759
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-11-14 17:41:31 -0800

    Remove report web vitals.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { UALProvider, withUAL } from "ual-reactjs-renderer";
 import { Anchor } from "ual-anchor";
 
@@ -46,8 +44,3 @@ root.render(
     <AppConsumer />
   </UALProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
